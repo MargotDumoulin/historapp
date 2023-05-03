93a3912029c00cba107d76f496fdf7cc3966e3a7
Snorloux <margot.dumoulin18@gmail.com>
2023-05-03 20:51:58 +0200

    Add beginning of collaboration

--- src/lib/components/Tiptap.svelte
+++ src/lib/components/Tiptap.svelte
@@ -1,16 +1,32 @@
-<script>
+<script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
 	import { Editor } from '@tiptap/core';
 	import StarterKit from '@tiptap/starter-kit';
+	import Collaboration from '@tiptap/extension-collaboration';
+	import { HocuspocusProvider } from '@hocuspocus/provider';
 
-	let element;
-	let editor;
+	let element: any;
+	let editor: any;
 
 	onMount(() => {
+		// Set up the Hocuspocus WebSocket provider
+		const provider = new HocuspocusProvider({
+			url: 'ws://0.0.0.0:1234',
+			name: 'example-document'
+		});
+
 		editor = new Editor({
-			element: element,
-			extensions: [StarterKit],
-			content: '<p>Hello World! 🌍️ </p>',
+			element,
+			extensions: [
+				StarterKit.configure({
+					// The Collaboration extension comes with its own history handling
+					history: false
+				}),
+				// Register the document with Tiptap
+				Collaboration.configure({
+					document: provider.document
+				})
+			],
 			onTransaction: () => {
 				// force re-render so `editor.isActive` works as expected
 				editor = editor;
